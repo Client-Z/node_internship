940b89291722aae5bb3c7a1a84c95c76025598e5
Denis Oleshchenko <denisoleshenko@gmail.com>
2019-09-09 17:22:50 +0300

    fix solution 7

--- hw1 to review/07.js
+++ hw1 to review/07.js
@@ -5,14 +5,22 @@ function deepestObjCopyEver(obj) {
 	if(!isObj(obj)) return obj;
 	let copyObj = {};
 	for(let prop in obj) {
-		copyObj[prop] = isObj(obj[prop]) ? deepestObjCopyEver(obj[prop]) : obj[prop];
+		// maybe it's not what you have expected but I suppose it's better then nothing 
+		if(obj[prop] instanceof Date) {
+			copyObj[prop] = new Date(obj[prop]); 
+		} else if(obj[prop] instanceof RegExp) {
+			copyObj[prop] = new RegExp(obj[prop]);
+		} else {
+			copyObj[prop] = isObj(obj[prop]) ? deepestObjCopyEver(obj[prop]) : obj[prop];
+		}
 	}
 	return copyObj;
 }
 
 let date = new Date(); // the Date objects has a prop with Symbol type. But, Symbolic properties do not participate in for..in loop.
+let regs = new RegExp();
 // date instanceof Date
-
+// console.log(date instanceof Date)
 const wow = {
 	'a': {
 		0: 0,
@@ -22,13 +30,14 @@ const wow = {
 				return 0;
 			}
 		},
-		3: date
+		3: date,
+		4: regs
 	}
 }
 
 // console.log(deepestObjCopyEver(wow));
 let newObj = deepestObjCopyEver(wow);
 // let's check out if it's really copied
-wow.a[0] = 10; // difference
-console.log('wow: ', wow);
+// wow.a[0] = 10; // difference
+// console.log('wow: ', wow);
 console.log('newObj: ', newObj)
