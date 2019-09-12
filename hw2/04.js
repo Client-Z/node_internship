0c9109607f03e163b8da8d3c57725377ea5adbd2
Denis Oleshchenko <denisoleshenko@gmail.com>
2019-09-12 13:39:10 +0300

    task 04, callback version

--- hw2/04.js
+++ hw2/04.js
@@ -17,40 +17,64 @@ const rolesList = [
 ];
 
 const dataBase = {
-  verifyUser: (username, password, callback) => {
-    // TODO check username and password
-  },
+  	verifyUser: (username, password, callback) => {
+		// TODO check username and password
+		let result = usersList.filter(obj => obj.username === username);
+		if(result.length && result[0].password === password) {
+			callback(null, {username, password});
+		} else {
+			callback(true, {username, password});
+		}
+	},
 
-  getRoles: (username, callback) => {
-    // TODO get roles array
-  },
+	getRoles: (username, callback) => {
+		// TODO get roles array
+		// find all roles of the current user
+		let rolesOfUser = rolesList.filter(obj => obj.username === username).map(el => el.role);
+		if(rolesOfUser.length) {
+			callback(null, rolesOfUser);
+		} else {
+			callback(true);
+		}
+	},
 
-  logAccess: (username, callback) => {
-    // TODO access if admin
-  },
+	logAccess: (username, callback) => {
+		// TODO access if admin
+		let rolesOfUser = rolesList.filter(obj => obj.username === username);
+		let isAdmin = rolesOfUser.length > 0 ? rolesOfUser.map(el => el.role === 'admin')[0] : false;
+		if(isAdmin) {
+			callback(null, 'Logging was allowed');
+		} else {
+			callback(true);
+		}
+	},
 };
 
 const verifyUser = function(username, password, callback) {
-  dataBase.verifyUser(username, password, (error, userInfo) => {
-    if (error) {
-      callback(error)
-    } else {
-      dataBase.getRoles(username, (error, roles) => {
-        if (error) {
-          callback(error)
-        } else {
-          dataBase.logAccess(username, (error) => {
-            if (error) {
-              callback(error);
-            } else {
-              callback(null, userInfo, roles);
-            }
-          })
-        }
-      })
-    }
-  })
+	dataBase.verifyUser(username, password, (error, userInfo) => {
+		if (error) {
+			callback(`User ${username} wasn't verified`);
+		} else {
+			callback(`User ${username} was verified`);
+			dataBase.getRoles(username, (error, roles) => {				
+				if (error) {
+					callback(`User ${username} has no roles`);
+				} else {
+					callback(`User ${username} has roles: ${roles}`);
+					dataBase.logAccess(username, (error) => {
+						if (error) {
+							callback(`User ${username} hasn't 'admin' role`);
+						} else {
+							callback(`User ${username} was successfully loged`)
+						}
+					})
+				}
+			})
+		}
+	})
 };
-
+verifyUser('Sam', '222', function(data) {
+	console.log(data);
+});
 // TODO rewrite with promises
 // TODO rewrite with async/await
