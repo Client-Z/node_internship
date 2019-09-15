200b45469c8e1512bc7f8a1ae7b3b866aa7c3ac7
Denis Oleshchenko <denisoleshenko@gmail.com>
2019-09-15 18:40:50 +0300

    id generation for 01-2

--- hw2/01-2.js
+++ hw2/01-2.js
@@ -2,19 +2,22 @@
 class Person {
 	constructor(name) {
 		this.name = name;
+		this.id = `i${(+new Date).toString(16)}`;
 	}
 }
 
 class Student extends Person {
 	constructor(name, studentId) {
 		super(name);
-		this.studentId = studentId;
+		this.studentId = studentId ? studentId : this.id;
 	}
 	show() {
 		return `${this.studentId}: ${this.name}`;
 	}
 }
 
-const student = new Student('Demian', 234);
+const student = new Student('Demian');
+const student1 = new Student('Alana', 234);
 
 console.log(student.show()); // `studentId: name`
+console.log(student1.show());
