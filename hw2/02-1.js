f55dda4544a5ba423f42196979a8f62932d5bab5
Denis Oleshchenko <denisoleshenko@gmail.com>
2019-09-15 18:53:07 +0300

    fixes for 02-1 according to comments

--- hw2/02-1.js
+++ hw2/02-1.js
@@ -1,39 +1,42 @@
 // **ES5**
 const Person = function(firstName, lastName) {
 	this.firstName = firstName;
-	this.lastName  = lastName;
-	this.namesHistory = [];
-	this.namesHistory.push(firstName);
+	this.lastName  = lastName;	
 };
 
 Person.prototype.showName = function() {
 	return `${this.firstName} ${this.lastName}`;
 }
-Person.prototype.changeName = function(newName) {
-	this.firstName = newName;
-	this.namesHistory.push(newName);
-}
 
 const Employee = function(firstName, lastName, position) {
 	Person.call(this, firstName, lastName);
 	this.position = position;
+	this.namesHistory = [];
 	this.positionsHistory = [];
+	this.namesHistory.push(firstName);
 	this.positionsHistory.push(position);
+	this.creationDate = new Date().toLocaleDateString();
+	this.id = `i${(+new Date).toString(8)}`;
 };
 
 // Using Object.create to manually set Employee's "parent".
 Employee.prototype = Object.create(Person.prototype);
 
 Employee.prototype.showNameAndPosition = function() {
-	return `${this.firstName} ${this.lastName}: ${this.position}`;
+	return `id: ${this.id}, date of creation: ${this.creationDate};\n ${this.firstName} ${this.lastName}: ${this.position}\n`;
+}
+Employee.prototype.showHistory = function() {
+	return { 'namesHistory': this.namesHistory, 'positionsHistory': this.positionsHistory };
+}
+Person.prototype.changeName = function(newName) {
+	this.firstName = newName;
+	this.namesHistory.push(newName);
 }
 Employee.prototype.changePosition = function(newPosition) {
 	this.position = newPosition;
 	this.positionsHistory.push(newPosition);
 }
-Employee.prototype.showHistory = function() {
-	return { 'namesHistory': this.namesHistory, 'positionsHistory': this.positionsHistory };
-}
+
 // result
 const employee = new Employee('Bruce', 'Wayne', 'Superhero');
 
