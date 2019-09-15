395d0818fd8a72cdf6d608839393792a7cc8c2ea
Denis Oleshchenko <denisoleshenko@gmail.com>
2019-09-15 19:04:15 +0300

    changes according to comments for 02-2 task

--- hw2/02-2.js
+++ hw2/02-2.js
@@ -13,9 +13,25 @@ class Employee extends Person {
 	constructor(firstName, lastName, position) {
 		super(firstName, lastName);
 		this.position = position;
+		this.namesHistory = [];
+		this.positionsHistory = [];
+		this.namesHistory.push(firstName);
+		this.positionsHistory.push(position);
+		this.id = `e${(+new Date).toString(16)}`;
+	}
+	changeName(newName) {
+		this.firstName = newName;
+		this.namesHistory.push(newName);
+	}
+	changePosition(newPosition) {
+		this.position = newPosition;
+		this.positionsHistory.push(newPosition);
 	}
 	showNameAndPosition() {
-		return `${this.firstName} ${this.lastName}: ${this.position}`;
+		return `${this.id}: ${this.firstName} ${this.lastName} - ${this.position}`;
+	}
+	showHistory() {
+		return { 'namesHistory': this.namesHistory, 'positionsHistory': this.positionsHistory };
 	}
 }
 
@@ -24,3 +40,8 @@ const employee = new Employee('Clark', 'Kent', 'journalist');
 
 console.log(employee.showName()); // `firstName lastName`
 console.log(employee.showNameAndPosition()); // `firstName lastName: position`
+
+employee.changeName('Barry');
+employee.changePosition('Driver');
+console.log(employee.showNameAndPosition());
+console.log(employee.showHistory());
