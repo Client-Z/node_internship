0c4e0968d475137bf783bee4df9757ef41f0e61d
Denis Oleshchenko <denisoleshenko@gmail.com>
2019-09-15 19:24:36 +0300

    [04-data] - the export was simplified

--- hw2/04-data.js
+++ hw2/04-data.js
@@ -3,16 +3,15 @@ const roles = {
 	USER: 'user',
 };
 
-const usersList = [
+module.exports.usersList = [
 	{ username: 'Bob', password: '111' },
 	{ username: 'Sam', password: '222' },
 	{ username: 'Matt', password: '333' }
 ];
 
-const rolesList = [
+module.exports.rolesList = [
 	{ username: 'Bob', role: roles.ADMIN },
 	{ username: 'Bob', role: roles.USER },
 	{ username: 'Sam', role: roles.USER },
 	{ username: 'Matt', role: roles.USER },
 ];
-module.exports = {usersList, rolesList};
