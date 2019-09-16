1c66d46471d70ad31297b7b756a53907ffb1b752
Denis Oleshchenko <denisoleshenko@gmail.com>
2019-09-16 19:38:55 +0300

    [Task-04-3] - fixes according to comments

--- hw2/04-async.js
+++ hw2/04-async.js
@@ -3,28 +3,19 @@ const {usersList, rolesList} = require("./04-data");
 const dataBaseAsync = {
   	verifyUser: async (username, password) => {
 		const result = usersList.filter(obj => obj.username === username);
-		if(result.length && result[0].password === password) {
-			return { username, password };
-		} else {
-			throw new Error(`User ${username} wasn't verified`);
-		}
+		if(result.length && result[0].password === password) return { username, password };
+		throw new Error(`User ${username} wasn't verified`);
 	},
 	getRoles: async ({username, password}) => {
 		let rolesOfUser = rolesList.filter(obj => obj.username === username).map(el => el.role);
 		const result = { username, password, rolesOfUser };
-		if(rolesOfUser.length) {
-			return result;
-		} else {
-			throw new Error(`User ${username} has no roles`);
-		}
+		if(rolesOfUser.length) return result;
+		throw new Error(`User ${username} has no roles`);
 	},
 	logAccess: async ({username, rolesOfUser}) => {
 		const isAdmin = rolesOfUser.length > 0 ? rolesOfUser.filter(el => el === 'admin')[0] : false;
-		if(isAdmin) {
-			return true;
-		} else {
-			throw new Error(`User ${username} hasn't 'admin' role`);
-		}
+		if(isAdmin) return true;
+		throw new Error(`User ${username} hasn't 'admin' role`);
 	},
 };
 
@@ -33,14 +24,12 @@ const verifyUserAsync = async (username, password) => {
 		const verifiedData = await dataBaseAsync.verifyUser(username, password);
 		const rolesData = await dataBaseAsync.getRoles(verifiedData);
 		const result = await dataBaseAsync.logAccess(rolesData);
-		if(result) {
-			console.log('Success!')
-			console.log(`Username: ${username}; Password: ${password}; \nUser has role(s): [${rolesData.rolesOfUser}]`);
-		}
+		if(result)
+			return `Username: ${username}; Password: ${password}; \nUser has role(s): [${rolesData.rolesOfUser}]`
 	} catch (e) {
-	  console.log(e)
+	  return e;
 	}
 }
-verifyUserAsync('Bob', '111');
-verifyUserAsync('Sam', '222');
-verifyUserAsync('Bob', '222');
+verifyUserAsync('Bob', '111').then(result => console.log(result));
+verifyUserAsync('Sam', '222').then(result => console.log(result));
+verifyUserAsync('Bob', '222').then(result => console.log(result));
