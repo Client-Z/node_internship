287209412896c12d11426f50ad43bd6f918f09b2
Denis Oleshchenko <denisoleshenko@gmail.com>
2019-09-18 13:42:18 +0300

    [Task-01-stack]

--- hw3/01-stack.js
+++ hw3/01-stack.js
@@ -1,33 +1,52 @@
 /* Stacks */
 
 function Stack() {
-  this.count = 0;
   this.storage = {};
+	this.count = 0;
 
+	this.isValid = (val) => val === undefined || Number.isNaN(val) || val === null ? false : true;
   // Adds a value onto the end of the stack
-  this.push = function(value) {};
-
+  this.push = (value) => {
+		if(this.isValid(value)) {
+			this.storage[this.count] = value;
+			this.count++;
+		} else {
+			console.error(`You can't add any indefinite value! It must be some object or primitive.`);
+		}
+	};
   // Removes and returns the value at the end of the stack
-  this.pop = function() {};
-
-  this.size = function() {};
-
+	this.pop = () => {
+		if(this.count) {
+			this.count--;
+			let item = this.storage[this.count];
+			delete this.storage[this.count];
+			return item;
+		} else {
+			console.error(`The stack is empty`);
+		}
+	}
+	// Returns the size of the stack
+  this.size = () => this.count;
   // Returns the value at the end of the stack
-  this.peek = function() {}
+	this.peek = () => this.storage[this.count - 1];
 }
 
 const myStack = new Stack();
 
+myStack.push(null);
+
 myStack.push(1);
 myStack.push(2);
+myStack.push(function func() { return 1; });
+myStack.push(new Date());
 
-console.log(myStack.peek()); // 2
-console.log(myStack.pop()); // 2
-console.log(myStack.peek()); // 1
+console.log(myStack.peek()); // some date
+console.log(myStack.pop()); // some date
+console.log(myStack.peek()); // func
 
 myStack.push("node.js");
 
-console.log(myStack.size()); // 2
+console.log(myStack.size()); // 4
 console.log(myStack.peek()); // node.js
 console.log(myStack.pop()); // node.js
-console.log(myStack.peek()); // 1
+console.log(myStack.peek()); // func
