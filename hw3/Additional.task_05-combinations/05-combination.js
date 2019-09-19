79dca9c878f3c922faef5336436f733a54719b7f
Denis Oleshchenko <denisoleshenko@gmail.com>
2019-09-19 18:05:07 +0300

    [Task 05] - validation was added

--- hw3/Additional.task_05-combinations/05-combination.js
+++ hw3/Additional.task_05-combinations/05-combination.js
@@ -12,24 +12,28 @@ const phoneDigits = {
 // The solution is slow ~ O(N^3), but it works :)
 // Although, I think here is minimum possible number of cycles
 const letterCombinations = (digits) => {
-	const combine = (a, b) => {
-		const resultArr = [];
-		a.forEach(el => b.forEach(item => resultArr.push(el + item)));
-		return resultArr;
+	if(new RegExp('^[2-9]+$').test(digits)) {
+		const combine = (a, b) => {
+			const resultArr = [];
+			a.forEach(el => b.forEach(item => resultArr.push(el + item)));
+			return resultArr;
+		}
+		let result = [];
+		for(let i = 0; i < digits.length - 1; i++) {
+			if(i === 0) {
+				result = combine(phoneDigits[digits[i]], phoneDigits[digits[i+1]])
+			} else {
+				result = combine(result, phoneDigits[digits[i+1]])
+			}	
+		}
+		return result;
+	} else {
+		console.log(`You should pass only digits! Except 0 and 1.`);
 	}
-	let result = [];
-	for(let i = 0; i < digits.length - 1; i++) {
-		if(i === 0) {
-			result = combine(phoneDigits[digits[i]], phoneDigits[digits[i+1]])
-		} else {
-			result = combine(result, phoneDigits[digits[i+1]])
-		}	
-	}
-	return result;
 };
 
 console.log(letterCombinations('23')); // [ 'ad', 'ae', 'af', 'bd', 'be', 'bf', 'cd', 'ce', 'cf' ]
-console.log(letterCombinations('234')); 
+console.log(letterCombinations('234'));
 /* 
 [ 'adg','adh', 'adi', 'aeg', 'aeh', 'aei', 'afg', 'afh', 'afi', 'bdg', 'bdh', 'bdi', 'beg', 'beh', 
   'bei', 'bfg', 'bfh', 'bfi', 'cdg', 'cdh', 'cdi', 'ceg', 'ceh', 'cei', 'cfg', 'cfh', 'cfi' ]
