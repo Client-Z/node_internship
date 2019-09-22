9022f82e894403f35182a0161571ed26d718e746
Denis Oleshchenko <denisoleshenko@gmail.com>
2019-09-22 14:29:27 +0300

    fix Task 04

--- hw3/04-priority-queue.js
+++ hw3/04-priority-queue.js
@@ -10,15 +10,19 @@ function PriorityQueue () {
 	// this method will push element to queue
 	this.enqueue = (el) => {
 		if(this.isValid(el)) {
-			if(this.isEmpty()) {
-				collection.push(el);
-			} else {
-				collection.push(el);
-				collection.sort((a, b) => b[1] - a[1]);
+			if(this.isEmpty()) collection.push(el);
+			else {
+				for(let i = 0; i < collection.length; i++) {
+					if(collection[i][1] < el[1]) {
+						collection.splice(i, 0, el);
+						break;
+					} else if(i === collection.length - 1) {
+						collection.push(el);
+						break;
+					}
+				}
 			}
-		} else {
-			console.error(`You can't add any indefinite value!`);
-		}
+		} else console.error(`You can't add any indefinite value!`);
 	};
 
 	// this method will remove element from queue
@@ -44,8 +48,8 @@ pq.enqueue(['Intern 5', 3]);
 pq.print(); // [ [ 'Intern 2', 3 ], [ 'Intern 1', 2 ], [ 'Intern 4', 2 ], [ 'Intern 3', 1 ] ]
 
 pq.dequeue();
-
-console.log(pq.front()); // [ 'Intern 1', 2 ]
-pq.print(); // [ [ 'Intern 1', 2 ], [ 'Intern 4', 2 ], [ 'Intern 3', 1 ] ]
+pq.enqueue(['Intern 0', 3]);
+console.log(pq.front()); // [ 'Intern 5', 3 ]
+pq.print(); // [ [ 'Intern 5', 3 ], [ 'Intern 0', 3 ], [ 'Intern 1', 2 ], ['Intern 4', 2 ], [ 'Intern 3', 1 ] ]
 
 // priorities: 3 - highest, 1 - lowest
